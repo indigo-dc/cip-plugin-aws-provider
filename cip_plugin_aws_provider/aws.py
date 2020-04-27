59895a507f7934afcfbfc4ec81c1f37db3d1c7aa
Pablo Orviz <orviz@ifca.unican.es>
2020-04-27 15:17:47 +0000

    Remove trailing whitespace

--- cip_plugin_aws_provider/aws.py
+++ cip_plugin_aws_provider/aws.py
@@ -11,9 +11,10 @@ except ImportError:
     msg = 'Cannot import boto3.'
     raise exceptions.AwsProviderException(msg)
 
+
 class AwsProvider(providers.BaseProvider):
     service_type = "compute"
-    goc_service_type = None    
+    goc_service_type = None
 
     def __init__(self, opts):
         super(AwsProvider, self).__init__(opts)
@@ -22,14 +23,14 @@ class AwsProvider(providers.BaseProvider):
             msg = ('You must provide a AWS Region')
             raise exceptions.AwsProviderException(msg)
 
-        if not opts.aws_access_key or not opts.aws_secret_key:            
+        if not opts.aws_access_key or not opts.aws_secret_key:
             msg = ('You must provide a tuple AWS access key / AWS secret key')
             raise exceptions.AwsProviderException(msg)
 
         self.aws_region_code = opts.aws_region_code
         self.aws_access_key = opts.aws_access_key
         self.aws_secret_key = opts.aws_secret_key
-        
+
         self.static = providers.static.StaticProvider(opts)
 
     def setup_logging(self):
@@ -43,7 +44,7 @@ class AwsProvider(providers.BaseProvider):
         log_level = logging.DEBUG if self.opts.debug else logging.WARNING
         for log in external_logs:
             logging.getLogger(log).setLevel(log_level)
-   
+
     def get_images(self, **kwargs):
     #    self.aws_service_name = 'ec2'
 
@@ -51,24 +52,24 @@ class AwsProvider(providers.BaseProvider):
  #           'image_name': None,
             'image_id': None,
             'image_location': None,
-            'image_marketplace_id': None,            
+            'image_marketplace_id': None,
             'image_os_name': None,
             'image_architecture' : None,
             'other_info': [],
         }
-        images = {}                
-        client = boto3.client('ec2', 
-                              region_name=self.aws_region_code, 
-                              aws_access_key_id=self.aws_access_key, 
+        images = {}
+        client = boto3.client('ec2',
+                              region_name=self.aws_region_code,
+                              aws_access_key_id=self.aws_access_key,
                               aws_secret_access_key=self.aws_secret_key)
 
         images = client.describe_images(ExecutableUsers=['all'],Filters=[{'Name': 'architecture', 'Values': ['i386']}])
-        for i in images['Images']:                               
-            print (i['ImageId'])       
+        for i in images['Images']:
+            print (i['ImageId'])
             print (i['Architecture'])
             print (i['ImageLocation'])
             print (i['ImageType'])
-            aux_img = copy.deepcopy(template)           
+            aux_img = copy.deepcopy(template)
             aux_img.update(i)
 
             # properties
@@ -80,32 +81,32 @@ class AwsProvider(providers.BaseProvider):
             #     v = image.get(opts_k)
             #     d_properties[k] = v
             # aux_img.update(d_properties)
-            
+
             aux_img.update({
-                'image_id': i['ImageId'],                                
+                'image_id': i['ImageId'],
                 'image_architecture': i['Architecture'],
                 'other_info': i['ImageLocation']+i['ImageType']
-            })            
+            })
             images[id] = aux_img
         return images
 
     @staticmethod
     def populate_parser(parser):
         parser.add_argument(
-            '--aws-region',            
+            '--aws-region',
             default=utils.env('AWS_DEFAULT_REGION'),
             dest = 'aws_region_code',
             help=('Specify AWS Region Code '
                   '(i. e, us-east-2, ap-south-1, eu-west-3...))'))
         parser.add_argument(
-            '--aws-access-key',            
-            default=utils.env('AWS_ACCESS_KEY_ID'), 
+            '--aws-access-key',
+            default=utils.env('AWS_ACCESS_KEY_ID'),
             dest = 'aws_access_key',
-            help=('Specify AWS Access Key ID'))                  
+            help=('Specify AWS Access Key ID'))
 
         parser.add_argument(
-            '--aws-secret-key',            
-            default=utils.env('AWS_SECRET_ACCESS_KEY'), 
+            '--aws-secret-key',
+            default=utils.env('AWS_SECRET_ACCESS_KEY'),
             dest = 'aws_secret_key',
             help=('Specify AWS Secret Access Key for'
                   ' the provided AWS Access Key ID'))
