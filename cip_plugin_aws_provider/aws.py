1edc798268a60361c473d7f1572da280c4d37260
Pablo Orviz <orviz@ifca.unican.es>
2020-04-29 16:00:55 +0000

    Get required image attributes from official CentOS EC2 AMIs

--- cip_plugin_aws_provider/aws.py
+++ cip_plugin_aws_provider/aws.py
@@ -1,5 +1,7 @@
 import copy
+import json
 import logging
+import re
 
 from cloud_info_provider import exceptions
 from cloud_info_provider import providers
@@ -45,50 +47,97 @@ class AwsProvider(providers.BaseProvider):
         for log in external_logs:
             logging.getLogger(log).setLevel(log_level)
 
+    def _normalize_image_values(self, d_images):
+        normalized_values = {
+            # image_os_type
+            'Linux/UNIX': 'linux',
+            'Windows': 'windows',
+            # image_architecture
+            'i386': 'i686'
+        }
+        d = {}
+        for k, v in d_images.items():
+            value = None
+            try:
+                value = normalized_values[v]
+            except Exception:
+                value = v
+            d[k] = value
+        return d
+
+    def _get_distro_version(self, image_name, distro):
+        os_regexp = {
+            'centos': 'CentOS Linux ([0-9]+) .+'
+        }
+        try:
+            version = re.search(os_regexp[distro], image_name).groups()[0]
+        except IndexError:
+            version = None
+        return version
+
     def get_images(self, **kwargs):
-    #    self.aws_service_name = 'ec2'
+        images = {}
 
         template = {
- #           'image_name': None,
+            'image_name': None,
             'image_id': None,
-            'image_location': None,
-            'image_marketplace_id': None,
             'image_os_name': None,
             'image_architecture' : None,
-            'other_info': [],
+            'image_marketplace_id' : None,
         }
-        images = {}
+        defaults = self.static.get_image_defaults(prefix=True)
         client = boto3.client('ec2',
                               region_name=self.aws_region_code,
                               aws_access_key_id=self.aws_access_key,
                               aws_secret_access_key=self.aws_secret_key)
 
-        images = client.describe_images(ExecutableUsers=['all'],Filters=[{'Name': 'architecture', 'Values': ['i386']}])
-        for i in images['Images']:
-            print (i['ImageId'])
-            print (i['Architecture'])
-            print (i['ImageLocation'])
-            print (i['ImageType'])
-            aux_img = copy.deepcopy(template)
-            aux_img.update(i)
+        # FIXME(orviz) Use filters from file
+        _filters = {
+            #"ubuntu":[
+            #    {"Name": "architecture", "Values": ["x86_64"]},
+            #    {"Name": "state", "Values": ["available"]},
+            #    {"Name": "root-device-type", "Values": ["ebs"]},
+            #    {"Name": "is-public", "Values": ["true"]},
+            #    {"Name": "name", "Values": ["ubuntu/images/*%s*"]},
+            #    #{'Name': 'name', 'Values': ['ubuntu/images/*2020*']},
+            #    {"Name": "owner-id", "Values": ["099720109477"]}
+            #],
+            "centos": [
+                {"Name": "architecture", "Values": ["x86_64"]},
+                {"Name": "state", "Values": ["available"]},
+                {"Name": "root-device-type", "Values": ["ebs"]},
+                {"Name": "is-public", "Values": ["true"]},
+                {"Name": "product-code", "Values": ["aw0evgkw8e5c1q413zgy5pjce"]},
+                {"Name": "owner-id", "Values": ["679593333241"]}
+            ]
+		}
 
-            # properties
-            # property_keys = [_opt for _opt in vars(self.opts)
-            #                  if _opt.startswith('property_image_')]
-            # d_properties = {}
-            # for k in property_keys:
-            #     opts_k = vars(self.opts)[k]
-            #     v = image.get(opts_k)
-            #     d_properties[k] = v
-            # aux_img.update(d_properties)
+        for _distro, _filter in _filters.items():
+            image_data = client.describe_images(ExecutableUsers=['all'],Filters=_filter)
+            for image in image_data['Images']:
+                img_id = image.get('ImageId')
 
-            aux_img.update({
-                'image_id': i['ImageId'],
-                'image_architecture': i['Architecture'],
-                'other_info': i['ImageLocation']+i['ImageType']
-            })
-            images[id] = aux_img
+                aux_img = copy.deepcopy(template)
+                aux_img.update(defaults)
+                aux_img.update(image)
+
+                _image_os_version = self._get_distro_version(image.get('Name'), _distro)
+                aux_img.update({
+                    'image_id': image.get('ImageId'),
+                    'image_name': image.get('Name'),
+                    'image_architecture': image.get('Architecture'),
+                    'image_os_type': image.get('PlatformDetails'),
+                    'image_os_name': _distro,
+                    'image_os_version': _image_os_version,
+                    'image_marketplace_id': image.get('ImageLocation'),
+                })
+                images[img_id] = self._normalize_image_values(aux_img)
+
         return images
+
+    def get_compute_endpoints(self, **kwargs):
+        images = self.get_images()
+        return {}
 
     @staticmethod
     def populate_parser(parser):
